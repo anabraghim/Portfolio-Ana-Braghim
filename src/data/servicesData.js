641f6d447a13d0e16f8a08cfb0809e55c9337f85
Ana Braghim <anajuliabraghim@gmail.com>
2024-09-01 00:47:26 -0300

    atualização de valores

--- src/data/servicesData.js
+++ src/data/servicesData.js
@@ -2,32 +2,22 @@ export const services = [
     {
         title: "Sites Institucionais",
         details: "Criação de sites para empresas locais apresentarem seus serviços, horários e localização.",
-        value: "R$ 400,00",
-    },
-    {
-        title: "Portfólios Online",
-        details: "Desenvolvimento de portfólios digitais para artistas, fotógrafos e profissionais exibirem seus trabalhos.",
-        value: "R$ 300,00",
+        value: "R$ 1000,00",
     },
     {
         title: "Landing Pages",
         details: "Criação de landing pages focadas em conversão para campanhas de marketing e promoções.",
-        value: "R$ 300,00",
+        value: "R$ 600,00",
     },
     {
         title: "Manutenção de Sites",
         details: "Serviços de atualização de conteúdo, segurança e performance para sites existentes.",
-        value: "R$ 200,00",
+        value: "a partir de orçamento",
     },
     {
-        title: "Aplicativos Web",
-        details: "Desenvolvimento de sistemas web como reservas e agendamentos de serviços.",
-        value: "R$ 500,00",
-    },
-    {
-        title: "Otimização para SEO",
-        details: "Serviços de SEO e melhoria de performance para melhor carregamento e classificação nos resultados de busca.",
-        value: "R$ 150,00",
+        title: "Sites de links",
+        details: "Site para bio de redes sociais onde se concentra suas principais informações, seus links importantes e suas redes sociais. Podendo conter seus principais serviços também.",
+        value: "a partir de orçamento",
     },
     {
         title: "Serviços Personalizados",
